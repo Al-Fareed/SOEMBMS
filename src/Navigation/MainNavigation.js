41a780edccd30c790f3719375c5e0ef5feca5c38
Al-Fareed <alfareedss472@gmail.com>
2023-05-26 21:25:04 +0530

    removed unwanted jsx files from nav

--- src/Navigation/MainNavigation.js
+++ src/Navigation/MainNavigation.js
@@ -1,17 +1,49 @@
-import React from 'react'
-import MainHeader from './MainHeader';
-import NavLinks from './NavLinks';
-const MainNavigation = () => {
+import React from "react";
+import "./MainNavigation.css";
+import { NavLink } from "react-router-dom/cjs/react-router-dom";
+const MainHeader = (props) => {
   return (
-    <MainHeader>
-      <h2 className="main-navigation__title">
-        Electricity Billing and Management
-      </h2>
-      <nav className='main-navigation__header-nav'>
-        <NavLinks/>
-      </nav>
-    </MainHeader>
-  )
-}
+    <header className="main-header">
+      <div className="head">
+        <h2>Electricity Billing and Management</h2>
+      </div>
+      <div className="nav-links-container">
+        <ul className="nav-links">
+          <li>
+            <NavLink to="/" exact>
+              HOME
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/Sell" exact>
+              SELL
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/Buy" exact>
+              BUY
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/login" exact>
+              LOGIN
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+      <div className="dropdown mob-nav">
+        <button class="dropbtn">
+          <i className="fa-sharp fa-solid fa-bars"></i>
+        </button>
+        <div className="dropdown-content">
+          <li><NavLink to="/" exact>HOME</NavLink></li>
+          <li><NavLink to="/Sell" exact>SELL</NavLink></li>
+          <li><NavLink to="/Buy" exact>BUY</NavLink></li>
+          <li><NavLink to="/login" exact>LOGIN</NavLink></li>
+        </div>
+      </div>
+    </header>
+  );
+};
 
-export default MainNavigation
+export default MainHeader;
