76e4631a79cda7781af15abf63c0a8697eb2c1ad
Al-Fareed <alfareedss472@gmail.com>
2023-05-27 12:55:10 +0530

    listing of users, styling header is remaining

--- src/Pages/Buy.js
+++ src/Pages/Buy.js
@@ -1,9 +1,66 @@
-import React from 'react'
-
+import React from "react";
+import './Buy.css';
 const Buy = () => {
+  const sellers = [
+    {
+      name: "Mark",
+      Phone: "7348842781",
+      Address: "Washington",
+      units: 230,
+    },
+    {
+      name: "Zuckerberg",
+      Phone: "8217789463",
+      Address: "Mala",
+      units: 580,
+    },
+    {
+      name: "Zuckerberg",
+      Phone: "8217789463",
+      Address: "Mala",
+      units: 580,
+    },
+    {
+      name: "Zuckerberg",
+      Phone: "8217789463",
+      Address: "Mala",
+      units: 580,
+    },
+    {
+      name: "Zuckerberg",
+      Phone: "8217789463",
+      Address: "Mala",
+      units: 580,
+    },
+    {
+      name: "Zuckerberg",
+      Phone: "8217789463",
+      Address: "Mala",
+      units: 580,
+    },
+    
+  ];
   return (
-    <div>Buy</div>
-  )
-}
+    <div>
+      <div className="heading">
+        <p>Name</p>
+        <p>Address</p>
+        <p>Units</p>
+        <p>Action</p>
+      </div>
+      {sellers.map((seller) => (
+        <div className="user-container">
+          <h2>{seller.name}</h2>
+          <p>{seller.Address}</p>
+          <p>{seller.units} units</p>
+          <div className="btn-container">
+            <button>View</button>
+            <button>Request</button>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
 
-export default Buy
+export default Buy;
