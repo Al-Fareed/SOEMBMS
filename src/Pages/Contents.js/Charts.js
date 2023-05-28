922b3cb33453413557dfa17718b69a3769087b1e
Al-Fareed <alfareedss472@gmail.com>
2023-05-28 23:33:33 +0530

    changing chart

--- src/Pages/Contents.js/Charts.js
+++ src/Pages/Contents.js/Charts.js
@@ -1,34 +1,11 @@
-import React from "react";
-import Chart from "react-apexcharts";
-import './Chart.css'
-// import { useState } from "react";
+import React from 'react'
+import { ChartComponent } from '@syncfusion/ej2-react-charts/src'
 const Charts = () => {
-  const state = {
-    options: {
-      chart: {
-        id: "basic-bar",
-      },
-      xaxis: {
-        categories: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov","Dec"],
-      },
-    },
-    series: [
-      {
-        name: "series-1",
-        data: [2051,750,4525,5000,2510,6578,4589,3652,4500,2002,5621,3652],
-      },
-    ],
-  }
   return (
-    <div className="chart-container">
-      <Chart
-              options={state.options}
-              series={state.series}
-              type="line"
-              width="500"
-            />
+    <div>
+      <ChartComponent></ChartComponent>
     </div>
-  );
-};
+  )
+}
 
-export default Charts;
+export default Charts
