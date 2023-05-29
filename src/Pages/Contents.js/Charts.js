39fd2acfebfea36bb6534b186da289fb9e66f153
Al-Fareed <alfareedss472@gmail.com>
2023-05-29 17:28:52 +0530

    removed chart, will be implementing different one

--- src/Pages/Contents.js/Charts.js
+++ src/Pages/Contents.js/Charts.js
@@ -1,9 +1,8 @@
 import React from 'react'
-import { ChartComponent } from '@syncfusion/ej2-react-charts/src'
 const Charts = () => {
   return (
     <div>
-      <ChartComponent></ChartComponent>
+      Chart
     </div>
   )
 }
