c82213a987ccf62f17f0fb266071769e66262283
Al-Fareed <alfareedss472@gmail.com>
2023-05-29 17:33:04 +0530

    added button for application of smart meter

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,12 +1,19 @@
-import React from 'react'
-import Charts from './Contents.js/Charts'
-import './Home.css'
+import React, { useState } from "react";
+import Charts from "./Contents.js/Charts";
+import "./Home.css";
 const Home = () => {
+  const [loggedIn,setLoggedIn]=useState(false);
   return (
-    <div className='chart-container' >
-      <Charts/>
-    </div>
-  )
-}
+    <React.Fragment>
+      {!loggedIn && <div className="applyLInk">
+        Request for Smart meter
+        <button className="smart-meter-button">Apply</button>
+      </div>}
+      <div className="chart-container">
+        <Charts />
+      </div>
+    </React.Fragment>
+  );
+};
 
-export default Home
+export default Home;
