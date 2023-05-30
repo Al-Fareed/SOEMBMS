2a2349535d730672ca1497fac8d06425e8fa5bf4
Al-Fareed <alfareedss472@gmail.com>
2023-05-30 23:29:43 +0530

    selecting the year will give you data of that year

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,31 +1,39 @@
 import React, { useState } from "react";
 import Charts from "./Contents.js/Charts";
 import "./Home.css";
+
 const Home = () => {
   const [loggedIn] = useState(true);
-  const month = [
-    "Jan",
-    "Feb",
-    "Mar",
-    "Apr",
-    "May",
-    "Jun",
-    "Jul",
-    "Aug",
-    "Sep",
-    "Oct",
-    "Nov",
-    "Dec",
-  ];
-  const y2001 = [350, 320, 410, 450, 580, 420, 463, 515, 415, 440, 410];
+  const month = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+  const year = [2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012];
 
-  const years = [
-    2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012,
-  ];
-  const reversedYear = [...years].reverse();
+  const reversedYear = [...year].reverse();
   const unitsPerYear = [
     2051, 750, 3525, 5000, 2510, 6578, 4589, 3652, 4500, 2002, 5621, 3652,
   ];
+  const years = [
+    { year: 2001, values: [528, 531, 590, 500, 510, 600, 540, 500, 124, 560, 450, 530] },
+    { year: 2002, values: [518, 521, 510, 560, 540, 667, 544, 520, 214, 590, 410, 580] },
+    { year: 2003, values: [518, 521, 510, 560, 540, 667, 544, 520, 214, 590, 410, 580] },
+    { year: 2004, values: [518, 521, 510, 560, 540, 667, 544, 520, 214, 590, 410, 580] },
+    { year: 2005, values: [518, 521, 510, 560, 540, 667, 544, 520, 214, 590, 410, 580] },
+    { year: 2006, values: [518, 521, 510, 560, 540, 667, 544, 520, 214, 590, 410, 580] },
+    { year: 2007, values: [518, 521, 510, 560, 540, 667, 544, 520, 214, 590, 410, 580] },
+    { year: 2008, values: [518, 521, 510, 560, 540, 667, 544, 520, 214, 590, 410, 580] },
+    { year: 2009, values: [518, 521, 510, 560, 540, 667, 544, 520, 214, 590, 410, 580] },
+    { year: 2010, values: [518, 521, 510, 560, 540, 667, 544, 520, 214, 590, 410, 580] },
+    { year: 2011, values: [518, 521, 510, 560, 540, 667, 544, 520, 214, 590, 410, 580] },
+    { year: 2012, values: [210, 255, 300, 180, 313, 275, 354, 320, 414, 290, 310, 280] },
+  ];
+  const lastYear = years[years.length - 1].year;
+
+  const [selectedYear, setSelectedYear] = useState(lastYear);
+
+  const handleYearChange = (event) => {
+    setSelectedYear(event.target.value);
+  };
+
+  const selectedYearValues = years.find((item) => item.year === parseInt(selectedYear))?.values || [];
 
   return (
     <div className="home">
@@ -36,19 +44,21 @@ const Home = () => {
         </div>
       )}
       <div className="yearly-chart">
-        <Charts categories={years} data={unitsPerYear} />
+        <Charts categories={year} data={unitsPerYear} />
       </div>
       <div className="monthly-chart">
         <div className="year-container">
           Select the year:&nbsp;
-          <select name="" id="">
-            {reversedYear.map((year)=>(
-              <option value={year}>{year}</option>
+          <select name="" id="" onChange={handleYearChange}>
+            {reversedYear.map((year) => (
+              <option key={year} value={year}>
+                {year}
+              </option>
             ))}
           </select>
         </div>
         <hr />
-        <Charts categories={month} data={y2001} />
+        <Charts categories={month} data={selectedYearValues} />
       </div>
     </div>
   );
