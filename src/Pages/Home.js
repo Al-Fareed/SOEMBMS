66dca32b64dfc7dda0f80fa15773a1138a472ad0
Al-Fareed <alfareedss472@gmail.com>
2023-05-30 20:40:56 +0530

    changes in logic implementation, shifted calendar

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -3,10 +3,27 @@ import Charts from "./Contents.js/Charts";
 import "./Home.css";
 const Home = () => {
   const [loggedIn] = useState(true);
-  const month = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
-  const unitsPerMonth = [350,320,410,450,580,420,463,515,415,440,410];
-  const year = [2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012 ];
-  const unitsPerYear = [2051, 750, 3525, 5000, 2510, 6578, 4589, 3652, 4500, 2002, 5621, 3652];
+  const month = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  const unitsPerMonth = [350, 320, 410, 450, 580, 420, 463, 515, 415, 440, 410];
+  const year = [
+    2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012,
+  ];
+  const unitsPerYear = [
+    2051, 750, 3525, 5000, 2510, 6578, 4589, 3652, 4500, 2002, 5621, 3652,
+  ];
 
   return (
     <div className="home">
@@ -16,16 +33,17 @@ const Home = () => {
           <button className="smart-meter-button">Apply</button>
         </div>
       )}
+      <div className="yearly-chart">
+        <Charts categories={year} data={unitsPerYear} />
+      </div>
       <div className="monthly-chart">
+        <div>
+          Select month and year
+          <input type="month"></input>
+        </div>
         <Charts categories={month} data={unitsPerMonth} />
       </div>
-        <div className="yearly-chart">
-          <div>
-            Select month and year <input type="month" id="bdaymonth" name="bdaymonth"></input>
-            
-          </div>
-          <Charts categories={year} data={unitsPerYear} />
-        </div>
+      
     </div>
   );
 };
