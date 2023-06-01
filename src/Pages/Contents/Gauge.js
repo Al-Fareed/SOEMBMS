d685dbb3442c37541e169130bcadf845b465efae
Al-Fareed <alfareedss472@gmail.com>
2023-06-01 17:40:27 +0530

    added last month description part

--- src/Pages/Contents/Gauge.js
+++ src/Pages/Contents/Gauge.js
@@ -1,18 +1,21 @@
 import React from "react";
 import GaugeChart from "react-gauge-chart";
-const Gauge = () => {
+const Gauge = (props) => {
+const  numOfLevels = 1000;
+const per = props.value/numOfLevels;
+
   return (
     <React.Fragment>
       <GaugeChart
         id="gauge-chart2"
-        nrOfLevels={420}
-        arcsLength={[0.33, 0.33, 0.33]}
-        colors={["#5BE12C", "#F5CD19", "#EA4228"]}
+        nrOfLevels={numOfLevels}
+        arcsLength={[0.25,0.25, 0.25, 0.25]}
+        colors={["lightgreen", "yellow","orange", "red"]}
         cornerRadius={0}
-        percent={0.20}
+        percent={per}
         arcPadding={0}
         textColor={'black'}
-        hideText='true'
+        hideText={true}
       />
     </React.Fragment>
   );
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,26 +1,25 @@
-import React, {useState } from "react";
+import React, { useState } from "react";
 import Charts from "./Contents/Charts";
 import Gauge from "./Contents/Gauge";
 import "./Home.css";
 const Home = () => {
   const [loggedIn] = useState(true);
   const month = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-
- 
   const years = [
-    { year: 2001, values: [457, 407, 446, 280, 399, 412, 290, 326, 226, 356, 453, 420] },
-    { year: 2002, values: [152, 361, 462, 438, 434, 325, 478, 235, 189, 243, 174, 261] },
-    { year: 2003, values: [170, 192, 247, 251, 220, 263, 307, 295, 188, 166, 292, 223] },
-    { year: 2004, values: [239, 300, 183, 281, 215, 205, 288, 239, 288, 271, 234, 242] },
-    { year: 2005, values: [175, 207, 156, 225, 174, 240, 176, 196, 309, 182, 288, 162] },
-    { year: 2006, values: [190, 265, 241, 187, 194, 167, 211, 248, 300, 252, 278, 191] },
-    { year: 2007, values: [235, 309, 221, 201, 212, 185, 162, 235, 293, 231, 228, 238] },
-    { year: 2008, values: [212, 229, 187, 283, 170, 272, 184, 191, 283, 183, 224, 150] },
-    { year: 2009, values: [232, 238, 160, 167, 189, 167, 226, 266, 245, 173, 260, 263] },
-    { year: 2010, values: [230, 167, 157, 228, 307, 275, 151, 247, 261, 259, 259, 296] },
-    { year: 2011, values: [166, 265, 294, 242, 212, 192, 199, 180, 298, 279, 305, 285] },
-    { year: 2012, values: [416, 195, 308, 239, 182, 504, 187, 437, 382, 491, 317, 204] },
+    { year: 2012, values: [457, 407, 446, 280, 399, 412, 290, 326, 226, 356, 453, 420] },
+    { year: 2013, values: [152, 361, 462, 438, 434, 325, 478, 235, 189, 243, 174, 261] },
+    { year: 2014, values: [170, 192, 247, 251, 220, 263, 307, 295, 188, 166, 292, 223] },
+    { year: 2015, values: [239, 300, 183, 281, 215, 205, 288, 239, 288, 271, 234, 242] },
+    { year: 2016, values: [175, 207, 156, 225, 174, 240, 176, 196, 309, 182, 288, 162] },
+    { year: 2017, values: [190, 265, 241, 187, 194, 167, 211, 248, 300, 252, 278, 191] },
+    { year: 2018, values: [235, 309, 221, 201, 212, 185, 162, 235, 293, 231, 228, 238] },
+    { year: 2019, values: [212, 229, 187, 283, 170, 272, 184, 191, 283, 183, 224, 150] },
+    { year: 2020, values: [232, 238, 160, 167, 189, 167, 226, 266, 245, 173, 260, 263] },
+    { year: 2021, values: [230, 167, 157, 228, 307, 275, 151, 247, 261, 259, 259, 296] },
+    { year: 2022, values: [166, 265, 294, 242, 212, 192, 199, 180, 298, 279, 305, 285] },
+    { year: 2023, values: [416, 195, 308, 239, 350] },
   ];
+
   const reversedYear = years.map((item) => item.year).reverse();
   const lastYear = years[years.length - 1].year;
 
@@ -29,28 +28,39 @@ const Home = () => {
   const handleYearChange = (event) => {
     setSelectedYear(event.target.value);
   };
-
-  const selectedYearValues = years.find((item) => item.year === parseInt(selectedYear))?.values || [];
+  // to the values of last arrays last values, representing previous month units consumed 
+  const thisYearValues = years.find((item)=> item.year === lastYear)?.values || [];
+  const unitsConsumedLastMon = thisYearValues[thisYearValues.length-1];
+  console.log('lastMonth', unitsConsumedLastMon);
+  
+  const selectedYearValues =
+    years.find((item) => item.year === parseInt(selectedYear))?.values || [];
 
   // to calculate total units in each year
-  let sumForAvg=0;
+  let sumForAvg = 0;
   const unitsPerYear = years.map((yearData) => {
-    const sum = yearData.values.reduce((accumulator, value) => accumulator + value, 0);
-     sumForAvg += sum;
-     return sum;
-    });
-    const avgUnitsPerMonth = sumForAvg / (years.length * 12);
-    
+    const sum = yearData.values.reduce(
+      (accumulator, value) => accumulator + value,
+      0
+    );
+    sumForAvg += sum;
+    return sum;
+  });
+  const avgUnitsPerMonth = Math.round(sumForAvg / (years.length * 12));
+  
   return (
     <div className="home">
-      {!loggedIn && (
+      {!loggedIn && 
         <div className="applyLink">
           Request for Smart meter
           <button className="smart-meter-button">Apply</button>
         </div>
-      )}
+      }
       <div className="yearly-chart">
-        <Charts categories={years.map((item) => item.year)} data={unitsPerYear} />
+        <Charts
+          categories={years.map((item) => item.year)}
+          data={unitsPerYear}
+        />
       </div>
       <div className="monthly-chart">
         <div className="year-container">
@@ -67,9 +77,20 @@ const Home = () => {
         <Charts categories={month} data={selectedYearValues} />
       </div>
       <div className="avgUnitsPerMon">
-        <Gauge />
-        <h3>{Math.round(avgUnitsPerMonth)} units</h3>
+        <Gauge
+        value={avgUnitsPerMonth} 
+        />
+        <h3>{avgUnitsPerMonth} units</h3>
         <h2>Average Units per Month </h2>
+      </div>
+
+      <div className="lastMonth">
+        <h2>Units consumed Last Month</h2>
+          <p>{unitsConsumedLastMon} units</p> 
+          <div className="paymentButton">
+            <button>View Bill</button>
+            <button>Pay</button>
+          </div>
       </div>
     </div>
   );
