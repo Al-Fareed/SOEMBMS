b934dea23fbff6a8b5183c36113c203aea108436
Al-Fareed <alfareedss472@gmail.com>
2023-06-02 20:17:18 +0530

    made chart responsive, needs to be checked

--- src/Pages/Contents/Charts.js
+++ src/Pages/Contents/Charts.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Chart from "react-apexcharts";
-import './Chart.css'
+import './Chart.css';
 const Charts = (props) => {
   const state = {
     options: {
@@ -18,13 +18,31 @@ const Charts = (props) => {
       },
     ],
   }
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    // Function to update screen width on window resize
+    const handleResize = () => {
+      setScreenWidth(window.innerWidth);
+    };
+
+    // Event listener for window resize
+    window.addEventListener("resize", handleResize);
+
+    // Clean up the event listener on component unmount
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+  const chartWidth = screenWidth > 768 ? 500 : "100%";
+
   return (
     <div className="chart-container">
       <Chart
               options={state.options}
               series={state.series}
               type="line"
-              width="500"
+              width={chartWidth}
             />
     </div>
   );
