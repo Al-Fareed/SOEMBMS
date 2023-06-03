77d060b65f33958a206df7956e05a202f6a77987
Al-Fareed <alfareedss472@gmail.com>
2023-06-03 17:06:34 +0530

    page to be rendered if the user is  not logged in

---
+++ src/Pages/Ads.js
@@ -0,0 +1,10 @@
+// If user is not logged in, then this page is rendered for advertising
+import React from 'react'
+
+const Ads = () => {
+  return (
+    <div>Ads</div>
+  )
+}
+
+export default Ads
