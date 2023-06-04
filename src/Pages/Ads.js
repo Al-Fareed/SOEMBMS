04e0085bf749b78fec549cfb02db47bc02e44d34
Al-Fareed <alfareedss472@gmail.com>
2023-06-04 12:17:42 +0530

    added images

--- src/Pages/Ads.js
+++ src/Pages/Ads.js
@@ -26,7 +26,7 @@ const Ads = () => {
       block: "start",
     });
   };
-const scrollToInsight = () => {
+  const scrollToInsight = () => {
     InsightRef.current.scrollIntoView({
       behavior: "smooth",
       block: "start",
@@ -48,27 +48,45 @@ const scrollToInsight = () => {
           is a cutting-edge solution to automate electricity billing, provide
           insight to track power consumption of each consumers.
         </div>
-        </div>
+      </div>
       <div className="objectives">
-      <img src={require("../images/y.jpg")} alt="Why" />
+        <img src={require("../images/y.jpg")} alt="Why" />
         <ul>
-          <li onClick={scrollToAutomatedMeterReading}>Automated meter reading and accurate billing</li>
+          <li onClick={scrollToAutomatedMeterReading}>
+            Automated meter reading and accurate billing
+          </li>
           <li onClick={scrollToInsight}>Insight on Electricity consumption</li>
           <li onClick={scrollToTrading}>Electricity trading</li>
           <li onClick={scrollToEnhancedSecurity}>Enhanced security</li>
         </ul>
       </div>
       <div ref={automatedMeterReadingRef} className="automated-meter-reading">
-      To overcome traditional approach of reading and billing of consumption of electricity, which takes labour work. This system will integrate smart metering technology to enable real-time monitoring and generation of bill.
+        <h1>Automated meter reading</h1>
+        <img src={require("../images/Automate.jpg")} alt="" />
+        To overcome traditional approach of reading and billing of consumption
+        of electricity, which takes labour work. This system will integrate
+        smart metering technology to enable real-time monitoring and generation
+        of bill.
       </div>
       <div ref={tradingRef} className="trading">
-      A consumer who has more than sufficient electricity can sell electricity as well as other user in this application can buy the listed users willing to sell their un consumed electricity.
+        <h1>Electricity Trading</h1>
+        <img src={require("../images/Buy-sell.avif")} alt="" />A consumer who
+        has more than sufficient electricity can sell electricity as well as
+        other user in this application can buy the listed users willing to sell
+        their un consumed electricity.
       </div>
       <div ref={InsightRef}>
-          hello
+      <img src={require("../images/meter-reading.jpg")} alt="" />
+        <h1>Insight on electricity consumption</h1>
+        hello
       </div>
       <div ref={enhancedSecurityRef} className="enhanced-security">
-      Main key feature of this application is enhanced security. The system employs blockchain technology to enhance security, ensuring secure transactions, protecting data integrity, and providing reliable user authentication.
+        <h1>Enhanced Security</h1>
+        <img src={require("../images/Security.png")} alt="" />
+        Main key feature of this application is enhanced security. The system
+        employs blockchain technology to enhance security, ensuring secure
+        transactions, protecting data integrity, and providing reliable user
+        authentication.
       </div>
     </React.Fragment>
   );
